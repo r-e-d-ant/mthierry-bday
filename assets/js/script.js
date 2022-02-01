149b7ee1e5ea26e29c583e5eca6f391b26be2f91
r-e-d-ant <mugishathierry65@gmail.com>
2022-02-01 15:52:08 +0200

    animation readded

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,16 @@ const bdayYear = document.querySelector('.year');
 bdayYear.textContent = 2002
 
 var yearPlus = 2002;
+var sec = 0;
 
 const giveYear = () => {
     if(yearPlus != 2022) {
         yearPlus += 1
+        sec += 1
         bdayYear.textContent = yearPlus
     }else {
         bdayYear.textContent = 2022
     }
 }
 
-// setInterval(giveYear, 1000);
+setInterval(giveYear, 1000);
